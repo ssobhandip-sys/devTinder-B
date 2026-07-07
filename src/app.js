38b5b981e1e85992898154d01d1f39c6b785bac1
Sobhandip Samanta <ssobhandip@gmail.com>
2026-07-08 01:30:21 +0530

    created get post routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 const express=require("express");
 require('dotenv').config();
 const app =express();
+//wild card route
+
 
 app.use("/test",(req,res)=>{
     res.send("Hello rom server 3000")
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello")
+app.get("/user",(req,res)=>{
+    res.send("user data send")
 })
-app.use("/",(req,res)=>{
-    res.send("Namaste node")
+
+app.post("/user",(req,res)=>{
+    res.send("data saved to the db")
 })
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server is listening to port ${process.env.PORT}`)
 })
