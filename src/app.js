c935897c06b9bf125dac6b797f0bc2b52cfc40b8
Sobhandip Samanta <ssobhandip@gmail.com>
2026-07-09 23:01:12 +0530

    play with multiple route handler in one route

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
-const express=require("express");
-require('dotenv').config();
-const app =express();
+const express = require("express");
+require("dotenv").config();
+const app = express();
 //wild card route
 
+app.use(
+  "/user",
+  (req, res,next) => {
+    console.log("1st route handler");
+    next();
+    //res.send("Hello rom server 3000");
+    
+  },
+  (req, res) => {
+    console.log("2nd route handler")
+    //res.send("2nd route handler");
+    next();
+  },
+);
 
-app.use("/test",(req,res)=>{
-    res.send("Hello rom server 3000")
-})
-
-app.get("/user",(req,res)=>{
-    res.send("user data send")
-})
-
-app.post("/user",(req,res)=>{
-    res.send("data saved to the db")
-})
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is listening to port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening to port ${process.env.PORT}`);
+});
