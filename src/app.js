71d4d37adf4d09abd7f57fd346f0571bd9432fac
Sobhandip Samanta <ssobhandip@gmail.com>
2026-07-09 23:36:51 +0530

    implements dummy auth middleware

--- src/app.js
+++ src/app.js
@@ -3,20 +3,27 @@ require("dotenv").config();
 const app = express();
 //wild card route
 
-app.use(
-  "/user",
-  (req, res,next) => {
-    console.log("1st route handler");
-    next();
-    //res.send("Hello rom server 3000");
-    
-  },
-  (req, res) => {
-    console.log("2nd route handler")
-    //res.send("2nd route handler");
-    next();
-  },
-);
+
+//handle auth middleware for all Get,Post ... requests
+app.use("/admin",(req,res,next)=>{
+    console.log("admin authorization is implemented")
+    const token="xyzzzzzz"
+    const isAdminAuthorize=token==="xyz";
+    if(!isAdminAuthorize){
+        res.status(401).send("admin is not authorized")
+    }else{
+        next();
+    }
+})
+
+
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("all data send")
+})
+
+app.get("/admin/deleteUse",(req,res)=>{
+    res.send("User deleted")
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening to port ${process.env.PORT}`);
