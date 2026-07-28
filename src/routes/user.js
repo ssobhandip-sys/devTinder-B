5600ce40655b8624231d024e071740cb7dcf9781
Sobhandip Samanta <ssobhandip@gmail.com>
2026-07-28 20:46:50 +0530

    Implemented user feed to show the new user on the feed

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,14 +2,17 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-userRouter.get("/user/requests/received",userAuth, async (req, res) => {
+const User = require("../models/user");
+
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender";
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId",["firstName","lastName","photoUrl","age","gender"]);
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.status(200).json({
       message: "Connection requests fetched for " + loggedInUser.firstName,
@@ -17,6 +20,72 @@ userRouter.get("/user/requests/received",userAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(400).send("Error while fetching requests :" + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const userConnections = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = userConnections.map((conn) => {
+      if (conn.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return conn.toUserId;
+      }
+      return conn.fromUserId;
+    });
+    res.status(200).json({
+      message: "Connection fetched for " + loggedInUser.firstName,
+      data: data,
+    });
+  } catch (err) {
+    res.status(400).send("Error while fetching connections :" + err.message);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    //user should see  except him in the feed
+    //his connection
+    //ignored people
+    //already sent connection request
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUserFromFeed = new Set();
+    connectionRequest.forEach((request) => {
+      hideUserFromFeed.add(request.fromUserId.toString());
+      hideUserFromFeed.add(request.toUserId.toString());
+    });
+    console.log(hideUserFromFeed);
+
+    const user = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+
+
+    res.status(200).json({
+      message: "User feed for " + loggedInUser.firstName,
+      data: user,
+    });
+  } catch (err) {
+    res
+      .status(400)
+      .send("Error while fetching the feed for user :" + err.message);
   }
 });
 
