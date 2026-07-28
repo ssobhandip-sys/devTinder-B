0f285710aa50d565bfb739b30784326334829fcd
Sobhandip Samanta <ssobhandip@gmail.com>
2026-07-28 21:05:40 +0530

    pagination is applied to the user feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -54,6 +54,10 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 5 ? 5 : limit;
+    const skip = (page - 1) * limit;
     //user should see  except him in the feed
     //his connection
     //ignored people
@@ -75,8 +79,10 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUserFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
-
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
 
     res.status(200).json({
       message: "User feed for " + loggedInUser.firstName,
