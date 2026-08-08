6db1c169aad0aa7e5308e2f2230abbaf8cc17454
Sobhandip Samanta <ssobhandip@gmail.com>
2026-08-08 16:51:14 +0530

    rejeted conection implement

--- devTinder-ui/src/components/Requests.jsx
+++ devTinder-ui/src/components/Requests.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { API_BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../utils/store/slices/requestSlice";
+import { addRequests,removeRequest } from "../utils/store/slices/requestSlice";
 
 const Requests = () => {
   const requests = useSelector((store) => store.requests);
@@ -18,11 +18,24 @@ const Requests = () => {
     } catch (err) {}
   };
 
+  const reviewRequest = async (status, _id) => {
+    try {
+      await axios.post(
+        API_BASE_URL + `/request/review/${status}/${_id}`,
+        {},
+        { withCredentials: true },
+      );
+      dispatch(removeRequest(_id))
+    } catch (err) {
+        console.error(err)
+    }
+  };
+
   useEffect(() => {
     fetchRequests();
   }, []);
   if (!requests) return;
-  if (requests.length === 0) return <h2>No Connection Found!!</h2>;
+  if (requests.length === 0) return <h2 className="flex justify-center my-10">No Request Found!!</h2>;
 
   return (
     <div className="text-center my-10">
@@ -54,8 +67,8 @@ const Requests = () => {
               {about && <p>{about}</p>}
             </div>
             <div>
-              <button className="btn btn-primary mx-2">Reject</button>
-              <button className="btn btn-secondary mx-2">Accept</button>
+              <button className="btn btn-primary mx-2" onClick={()=>reviewRequest("rejected",req._id)}>Reject</button>
+              <button className="btn btn-secondary mx-2" onClick={()=>reviewRequest("accepted",req._id)}>Accept</button>
             </div>
           </div>
         );
--- devTinder-ui/src/utils/store/slices/requestSlice.js
+++ devTinder-ui/src/utils/store/slices/requestSlice.js
@@ -5,9 +5,13 @@ const requestSlice = createSlice({
   initialState: null,
   reducers: {
     addRequests: (state, action) => action.payload,
+    removeRequest:(state,action)=>{
+        const newReqArr=state.filter((r)=>r._id!==action.payload);
+        return newReqArr;
+    }
   },
 });
 
-export const {addRequests}=requestSlice.actions;
+export const {addRequests,removeRequest}=requestSlice.actions;
 
 export default requestSlice.reducer;
